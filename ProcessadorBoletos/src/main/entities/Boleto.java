0b14a54b35ff065797870aaeed73c180f3eb4519
Luciano Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2023-04-12 09:43:19 -0300

    feat: Cria entidade Boleto

--- ProcessadorBoletos/src/main/entities/Boleto.java
+++ ProcessadorBoletos/src/main/entities/Boleto.java
@@ -1,6 +1,32 @@
 package main.entities;
 
+import java.time.LocalDate;
+
 public class Boleto {
+
+    private String codigo;
+    private LocalDate data;
+    private Float valorPago;
+
     public Boleto() {
     }
+
+    public Boleto(String codigo, LocalDate data, Float valorPago) {
+        this.codigo = codigo;
+        this.data = data;
+        this.valorPago = valorPago;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public Float getValorPago() {
+        return valorPago;
+    }
+
 }
