f6dc5b5803467c72ed881e29dd3c2731f889a043
Luciano Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2023-04-12 09:42:03 -0300

    feat: Cria entidade Fatura

--- ProcessadorBoletos/src/main/entities/Fatura.java
+++ ProcessadorBoletos/src/main/entities/Fatura.java
@@ -1,6 +1,31 @@
 package main.entities;
 
+import java.time.LocalDate;
+
 public class Fatura {
+    
+    private String nome;
+    private LocalDate data;
+    private Float valorTotal;
+
+    public Fatura(String nome, LocalDate data, Float valorTotal) {
+        this.nome = nome;
+        this.data = data;
+        this.valorTotal = valorTotal;
+    }
+
     public Fatura() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public Float getValorTotal() {
+        return valorTotal;
     }
 }
