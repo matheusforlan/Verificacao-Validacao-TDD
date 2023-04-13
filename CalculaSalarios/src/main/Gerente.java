9b4c99730685eea1872e37373531e70c93102100
matheusforlan <matheus.andrade@ccc.ufcg.edu.br>
2023-04-12 23:05:11 -0300

    Adicionando código da classe Gerente a partir dos testes

---
+++ CalculaSalarios/src/main/Gerente.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Gerente implements Cargo {
+    public double calcularSalarioLiquido(double salarioBase) {
+        if (salarioBase >= 5000) {
+            return salarioBase * 0.7;
+        } else {
+            return salarioBase * 0.8;
+        }
+    }
+}
--- CalculaSalarios/src/tests/GerenteTest.java
+++ CalculaSalarios/src/tests/GerenteTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import main.Gerente;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
