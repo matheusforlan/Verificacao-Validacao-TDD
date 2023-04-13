1ecef5320afcf1366cd612289787f5838a5715c3
matheusforlan <matheus.andrade@ccc.ufcg.edu.br>
2023-04-12 22:23:33 -0300

    Criando testes para a classe de desenvolvedor

--- CalculaSalarios/src/tests/DesenvolvedorTest.java
+++ CalculaSalarios/src/tests/DesenvolvedorTest.java
@@ -1,2 +1,21 @@
-package tests;public class DesenvolvedorTest {
+package tests;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DesenvolvedorTest {
+
+    @Test
+    public void testCalcularSalarioLiquidoSalarioMaiorQue3000() {
+        Desenvolvedor desenvolvedor = new Desenvolvedor();
+        double salarioLiquido = desenvolvedor.calcularSalarioLiquido(4000.0);
+        assertEquals(3200.0, salarioLiquido, 0.001);
+    }
+
+    @Test
+    public void testCalcularSalarioLiquidoSalarioMenorQue3000() {
+        Desenvolvedor desenvolvedor = new Desenvolvedor();
+        double salarioLiquido = desenvolvedor.calcularSalarioLiquido(2500.0);
+        assertEquals(2250.0, salarioLiquido, 0.001);
+    }
 }
