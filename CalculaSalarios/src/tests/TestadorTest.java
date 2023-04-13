7181870e2e8f16dcced8bb29940a86f9ec8da2d0
matheusforlan <matheus.andrade@ccc.ufcg.edu.br>
2023-04-12 22:53:29 -0300

    Adicionando testes da classe Testador

---
+++ CalculaSalarios/src/tests/TestadorTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestadorTest {
+
+    @Test
+    public void testCalcularSalarioLiquidoSalarioMaiorQue2000() {
+        Testador testador = new Testador();
+        double salarioLiquido = testador.calcularSalarioLiquido(3000.0);
+        assertEquals(2250.0, salarioLiquido, 0.001);
+    }
+
+    @Test
+    public void testCalcularSalarioLiquidoSalarioMenorQue2000() {
+        Testador testador = new Testador();
+        double salarioLiquido = testador.calcularSalarioLiquido(1500.0);
+        assertEquals(1275.0, salarioLiquido, 0.001);
+    }
+}
