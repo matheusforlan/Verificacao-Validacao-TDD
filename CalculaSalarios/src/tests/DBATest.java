c7f179ae390dace9a02d353425a0eab85dd3885d
matheusforlan <matheus.andrade@ccc.ufcg.edu.br>
2023-04-12 22:44:24 -0300

    Adicionando testes do DBA

---
+++ CalculaSalarios/src/tests/DBATest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DBATest {
+
+    @Test
+    public void testCalcularSalarioLiquidoSalarioMaiorQue2000() {
+        DBA dba = new DBA();
+        double salarioLiquido = dba.calcularSalarioLiquido(3000.0);
+        assertEquals(2250.0, salarioLiquido, 0.001);
+    }
+
+    @Test
+    public void testCalcularSalarioLiquidoSalarioMenorQue2000() {
+        DBA dba = new DBA();
+        double salarioLiquido = dba.calcularSalarioLiquido(1500.0);
+        assertEquals(1275.0, salarioLiquido, 0.001);
+    }
+}
+
